5b5c8620796296a4bed98d490eb09711678419c7
Morgane Bentzinger <mo.bentzinger@gmail.com>
2022-02-20 17:13:42 -0500

    Bis

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Pokemon pikachu = new Pokemon("Pikachu", 200, 55);
+        Pokemon jigglypuff = new Pokemon("Jigglypuff", 115, 45);
+
+        pikachu.setTarget(jigglypuff);
+        jigglypuff.setTarget(pikachu);
+
+        Thread t1 = new Thread(pikachu);
+        Thread t2 = new Thread(jigglypuff);
+
+        t1.start();
+        t2.start();
+    }
 }
--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Pokemon {
+public class Pokemon implements Runnable {
+    private String name;
+    private int hp;
+    private int attackPower;
+    private Pokemon target;
+
+    public Pokemon(String name, int hp, int attackPower) {
+        this.name = name;
+        this.hp = hp;
+        this.attackPower = attackPower;
+    }
+
+    public void setTarget(Pokemon target) {
+        this.target = target;
+    }
+
+    @Override
+    public void run() {
+        while(this.hp > 0 && target.hp > 0) {
+            target.hp -= this.attackPower;
+            System.out.println(this.name + " attacks " + target.name + ". " + target.name + "'s HP is now " + target.hp + ".");
+        }
+
+        try{
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        if(target.hp <= 0) {
+            System.out.println(this.name + " won against " + target.name + ".");
+        }
+    }
 }
